4230a5e46d779a290ee8370f9da36681fe08ad58
pawurb <p.urbanek89@gmail.com>
2013-11-08 17:59:18 +0100

    works like it should

--- scripts/search.js
+++ scripts/search.js
@@ -1,26 +1,29 @@
-function SearchData(fieldId, url){
-  this.fieldId = fieldId;
-  this.url = url;
-}
+(function(){
+  function SearchData(fieldId, url){
+    this.fieldId = fieldId;
+    this.url = url;
+  }
 
-var github = new SearchData('search-github', "https://github.com/search?q=");
-var rails = new SearchData('search-rails', "http://api.rubyonrails.org/?q=");
-var ruby = new SearchData('search-ruby', "http://ruby-doc.com/search.html?&q=");
-var gems = new SearchData('search-gems', "http://rubygems.org/search?&query=");
-
-window.onload = function() {
-  var searchGitHub = document.getElementById('search-github');
-  var searchRails = document.getElementById('search-rails');
-  var searchRuby = document.getElementById('search-ruby');
-  var searchGems = document.getElementById('search-gems');
-
-  searchBox.onkeypress = function(e){
-    var searchURL = "https://github.com/search?q=" + this.value;
-
-    if (e.keyCode == 13) {
-      chrome.tabs.update({ url: searchURL }, function(){
-        window.close();
-      });
+  var setSearchEvent = function(data){
+    var searchBox = document.getElementById(data.fieldId);
+    searchBox.onkeypress = function(e){
+      var searchURL = data.url + this.value;
+      if (e.keyCode == 13) {
+        chrome.tabs.update({ url: searchURL }, function(){
+          window.close();
+        });
+      }
     }
   }
-}
+
+  var github = new SearchData('search-github', "https://github.com/search?q=");
+  var rails = new SearchData('search-rails', "http://api.rubyonrails.org/?q=");
+  var ruby = new SearchData('search-ruby', "http://ruby-doc.com/search.html?&q=");
+  var gems = new SearchData('search-gems', "http://rubygems.org/search?&query=");
+
+  var searchObjects = [github, rails, ruby, gems];
+
+  for(var i = 0; i < searchObjects.length; i=i+1){
+    setSearchEvent(searchObjects[i])
+  }
+})();
